0367c724c900dede4198492a2b3c7a009a650db5
Aymen ASSAF <aymen.assaf@edu.uca.ma>
2024-03-12 18:22:44 +0000

    bonus completed 100%

--- client_bonus.c
+++ client_bonus.c
@@ -6,53 +6,72 @@
 /*   By: aassaf <aassaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:28:06 by aassaf            #+#    #+#             */
-/*   Updated: 2024/03/10 22:59:34 by aassaf           ###   ########.fr       */
+/*   Updated: 2024/03/11 20:30:07 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void handle_ack(int signal)
+static void	handle_ack(int signal)
 {
-    if (signal == SIGUSR2)
-        printf("Message received successfully!\n");
+	if (signal == SIGUSR1)
+	{
+		printf("Message received successfully!\n");
+		exit(0);
+	}
+	if (signal == SIGUSR2)
+		exit(1);
 }
 
-void send_signal(int pid, unsigned char character)
+void	send_char(int pid, char character)
 {
-    int i;
+	int	i;
 
-    i = 7;
-    while (i >= 0)
-    {
-        if ((character >> i) & 1)
-                kill(pid, SIGUSR1);
-        else
-                kill(pid, SIGUSR2);
-        i--;
-        usleep(400);
-    }
+	i = 7;
+	while (i >= 0)
+	{
+		if ((character >> i) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i--;
+		usleep(400);
+	}
 }
 
-int main(int ac, char **av)
+void	send_string(int pid, char *string)
 {
-    int pid;
-    int i;
+	int	i;
 
-    i = 0;
-    if(ac != 3)
-    {
-        printf("Usage: ./client_bonus [PID] [string]\n");
-        return 1;
-    }
-    pid = atoi(av[1]);
-        signal(SIGUSR2, handle_ack);
-    while (av[2][i] != '\0')
-    {
-        signal(SIGUSR1, handle_ack);
-        signal(SIGUSR2, handle_ack);
-        send_signal(pid, (unsigned char)av[2][i]);
-        i++;
-    }
-    return 0;
+	i = 0;
+	signal(SIGUSR2, handle_ack);
+	signal(SIGUSR1, handle_ack);
+	while (string[i])
+	{
+		send_char(pid, string[i]);
+		i++;
+	}
+	send_char(pid, '\0');
+}
+
+int	main(int ac, char **av)
+{
+	int	pid;
+	int	i;
+
+	if (ft_isdigit(ft_atoi(av[1])) == 0)
+		exit(1);
+	i = 0;
+	if (ac != 3)
+	{
+		printf("ERROR: ./client_bonus [PID] [string]\n");
+		return (1);
+	}
+	pid = ft_atoi(av[1]);
+	if (pid == -1)
+		return (-1);
+	send_string(pid, av[2]);
+	while (1)
+		;
+	return (0);
 }
--- server_bonus.c
+++ server_bonus.c
@@ -6,49 +6,48 @@
 /*   By: aassaf <aassaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:28:16 by aassaf            #+#    #+#             */
-/*   Updated: 2024/03/10 22:38:32 by aassaf           ###   ########.fr       */
+/*   Updated: 2024/03/12 18:21:04 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void handle_signal(int signal, siginfo_t *info, void *s)
+void	handle_signal(int signal, siginfo_t *info, void *s)
 {
-    static unsigned char buff = 0;
-    static int i = 0;
-    (void)info;
-    (void)s;
-    if (signal == SIGUSR1)
-    {
-        buff = buff << 1 | 1;
-        i++;
-    }
-    else if(signal == SIGUSR2)
-    {
-        buff = buff << 1;
-        i++;
-    }
-    if (i == 8)
-    {
-        write(1, &buff, 1);
-        i = 0;
-        buff = 0;
-        kill(info->si_pid, SIGUSR2);
-    }
+	static unsigned char	buff = 0;
+	static int				i = 0;
+	int						pid;
+
+	pid = info->si_pid;
+	(void)s;
+	if (signal == SIGUSR1)
+		buff = buff << 1 | 1;
+	else if (signal == SIGUSR2)
+		buff = buff << 1 | 0;
+	i++;
+	if (i == 8)
+	{
+		write(1, &buff, 1);
+		if (buff == '\0')
+			kill(pid, SIGUSR1);
+		i = 0;
+		buff = 0;
+	}
 }
 
-int main(void)
+int	main(int ac, char **av)
 {
-    printf("Server PID: %d\n", getpid());
-    usleep(50);
-    struct sigaction sa;
-    sa.sa_sigaction = handle_signal;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    while (1)
-    {
-        sigaction(SIGUSR1, &sa, NULL);
-        sigaction(SIGUSR2, &sa, NULL);
-        pause();
-    }
+	struct sigaction	sa;
+
+	(void)av;
+	if (ac > 1)
+		exit(1);
+	printf("Server PID: %d\n", getpid());
+	usleep(50);
+	sa.sa_sigaction = handle_signal;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) ==
+		-1);
+		return (-1);
+	while (1)
+		pause();
 }
